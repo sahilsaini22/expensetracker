67786c1cbe45142227e1e2026a73cbd67981fc26
sahilsaini22 <sahilsaini_22@outlook.com>
2022-04-12 17:41:48 +0200

    added basic layout for the app

--- routes.py
+++ routes.py
@@ -6,3 +6,12 @@ from flask import render_template
 @app.route('/index')
 def index():
     return render_template('index.html')
+
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
